f8706db0fad1de5955290e06fcf19978f6410558
marcossilva <marcos.dev1998@gmail.com>
2022-09-01 16:24:55 -0300

    Post synchronization with Action Cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identify_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      verified_user = env['warden'].user
+
+      verified_user || reject_unauthorized_connection
+    end
   end
 end
